97a06790bd399c98ef9f9537845e7fd8098ed123
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 18:31:50 +0545

    Add JaxRsActivator and Entity Manager Resource

---
+++ src/main/java/com/lftechnology/vyaguta/resource/Resources.java
@@ -0,0 +1,16 @@
+package com.lftechnology.vyaguta.resource;
+
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ *
+ */
+public class Resources {
+  @Produces
+  @PersistenceContext
+  private EntityManager em;
+}
