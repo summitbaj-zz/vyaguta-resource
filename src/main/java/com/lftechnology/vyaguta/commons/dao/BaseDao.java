02ac0db5d39af3a47d53a900a5aa7ac28a11a8a8
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 13:31:28 +0545

    integrate vyaguta-commons

---
+++ src/main/java/com/lftechnology/vyaguta/commons/dao/BaseDao.java
@@ -0,0 +1,41 @@
+package com.lftechnology.vyaguta.commons.dao;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import com.lftechnology.vyaguta.commons.pojo.ErrorMessage;
+
+/**
+ * @author Anish Krishna Manandhar<anishmanandhar@lftechnology.com>
+ * 
+ * @author Kailash Bijayananda <kailashraj@lftechnology.com>
+ *
+ */
+public abstract class BaseDao {
+
+    @Inject
+    protected EntityManager em;
+
+    /**
+     * Construct proper dao exception messages
+     * 
+     * @param throwable
+     * @return
+     */
+    public static ErrorMessage constructErrorMessage(Throwable throwable) {
+        String message = ExceptionUtils.getRootCauseMessage(throwable);
+        ErrorMessage errorMessage = new ErrorMessage("error", message);
+        if (message.contains("duplicate key value violates unique constraint")) {
+            message = StringUtils.substringAfter(message, "Detail: Key ");
+            String[] messages = message.split("=");
+            if (messages != null && messages.length > 1) {
+                errorMessage.setField(messages[0].replaceAll("\\(", "").replaceAll("\\)", ""));
+                errorMessage.setMessage(messages[1]);
+            }
+        }
+        return errorMessage;
+    }
+}
---
+++ src/main/java/com/lftechnology/vyaguta/commons/jpautil/UserSerializer.java
@@ -0,0 +1,21 @@
+package com.lftechnology.vyaguta.commons.jpautil;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.lftechnology.vyaguta.commons.pojo.User;
+
+/**
+ * 
+ * @author Kailash Bijayananda <kailashraj@lftechnology.com>
+ *
+ */
+public class UserSerializer extends JsonSerializer<User> {
+
+    public void serialize(User user, JsonGenerator generator, SerializerProvider provider) throws IOException, JsonProcessingException {
+        generator.writeString(user.getId());
+    }
+}
