ef82f49f5d4bb9d7ecc52544693e61e9c329b27a
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 15:04:33 +0545

    Create ProjectTypeService interface

---
+++ src/main/java/com/lftechnology/vyaguta/resource/service/ProjectTypeService.java
@@ -0,0 +1,12 @@
+package com.lftechnology.vyaguta.resource.service;
+
+import com.lftechnology.vyaguta.commons.service.CrudService;
+import com.lftechnology.vyaguta.resource.entity.ProjectType;
+
+/**
+ * 
+ * @author achyut <achyutpokhrel@lftechnology.com>
+ *
+ */
+public interface ProjectTypeService extends CrudService<ProjectType, String> {
+}
