8c6cc2b08a0e7c6b77b8ca8e89bff509dee42192
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 15:21:54 +0545

    Test case for ProjectTypeServiceImpl

---
+++ src/test/java/com/lftechnology/vyaguta/resource/service/impl/ProjectTypeServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.lftechnology.vyaguta.resource.service.impl;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.lftechnology.vyaguta.commons.pojo.User;
+import com.lftechnology.vyaguta.resource.dao.ProjectTypeDao;
+import com.lftechnology.vyaguta.resource.entity.ProjectType;;
+
+public class ProjectTypeServiceImplTest {
+  @InjectMocks
+  private ProjectTypeServiceImpl projectTypeServiceImpl;
+
+  @Mock
+  private ProjectTypeDao projectTypeDao;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void testSaveWhenProjectTypeIsValidObject() {
+    // arrange
+    ProjectType projectType = this.buildProjectType();
+    Mockito.when(projectTypeDao.save(projectType)).thenReturn(projectType);
+
+    // act
+    ProjectType resultProjectType = projectTypeServiceImpl.save(projectType);
+
+    // assert
+    Mockito.verify(projectTypeDao).save(projectType);
+    assertThat(resultProjectType, is(projectType));
+  }
+
+  @Test
+  public void testSaveWhenProjectTypeIsNull() {
+    // arrange
+    Mockito.when(projectTypeDao.save(null)).thenReturn(null);
+
+    // act
+    ProjectType resultProjectType = projectTypeServiceImpl.save(null);
+
+    // assert
+    Mockito.verify(projectTypeDao).save(null);
+
+  }
+
+  private ProjectType buildProjectType() {
+    ProjectType projectType = new ProjectType();
+    User user = buildUser();
+    projectType.setId("1");
+    projectType.setTitle("Test Title");
+    projectType.setCreatedBy(user);
+    projectType.setUpdatedAt(LocalDateTime.now());
+    projectType.setCreatedAt(LocalDateTime.now());
+    return projectType;
+  }
+
+  private User buildUser() {
+    User user = new User();
+    user.setEmail("achyutpokhrel@lftechnology.com");
+    user.setId("12345");
+    user.setName("achyut pokhrel");
+    return user;
+  }
+}
+
