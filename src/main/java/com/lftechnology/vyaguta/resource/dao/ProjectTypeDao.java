acfd270d4daad3bd6fb00d89feef2fcfbba00f79
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 15:02:42 +0545

    Create ProjectType Dao

---
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/ProjectTypeDao.java
@@ -0,0 +1,13 @@
+package com.lftechnology.vyaguta.resource.dao;
+
+import com.lftechnology.vyaguta.commons.dao.CrudDao;
+import com.lftechnology.vyaguta.resource.entity.ProjectType;
+
+/**
+ * 
+ * @author achyut <achyutpokhrel@lftechnology.com>
+ *
+ */
+public interface ProjectTypeDao extends CrudDao<ProjectType, String> {
+
+}
