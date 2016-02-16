5745f54180da6d94bab8a0b7100070dbeea9a510
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-16 13:53:35 +0545

    Entity class for Project Type

---
+++ src/main/java/com/lftechnology/vyaguta/resource/entity/ProjectType.java
@@ -0,0 +1,30 @@
+package com.lftechnology.vyaguta.resource.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.lftechnology.vyaguta.commons.entity.BaseEntity;
+
+/**
+ * 
+ * @author achyut <achyutpokhrel@lftechnology.com>
+ *
+ */
+@Entity
+@Table(name = "project_types")
+public class ProjectType extends BaseEntity implements Serializable {
+
+  private static final long serialVersionUID = 832909736494541803L;
+
+  private String title;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
