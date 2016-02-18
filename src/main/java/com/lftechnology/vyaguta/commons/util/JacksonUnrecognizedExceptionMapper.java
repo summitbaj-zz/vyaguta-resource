1f519057104d44eae055293fea4576c29e381fc0
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-18 09:54:14 +0545

    Add Json field unrecognized exception mapper

---
+++ src/main/java/com/lftechnology/vyaguta/commons/util/JacksonUnrecognizedExceptionMapper.java
@@ -0,0 +1,29 @@
+package com.lftechnology.vyaguta.commons.util;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.lftechnology.vyaguta.commons.pojo.ErrorMessage;
+
+/**
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ *
+ */
+@Provider
+public class JacksonUnrecognizedExceptionMapper implements ExceptionMapper<UnrecognizedPropertyException> {
+
+  @Override
+  public Response toResponse(UnrecognizedPropertyException exception) {
+    System.out.println(exception.getMessage());
+    ErrorMessage errorMessage = new ErrorMessage(
+        StringUtils.substringBefore(exception.getMessage().replace("\"", "\'"), "("));
+    return Response.status(Response.Status.BAD_REQUEST).entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+  }
+
+}
