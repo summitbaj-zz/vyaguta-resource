bee199390c9ba4f8a2fb115bf9c706bc4c7d6b00
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-18 09:44:41 +0545

    Change error message to single field

--- src/main/java/com/lftechnology/vyaguta/commons/dao/BaseDao.java
+++ src/main/java/com/lftechnology/vyaguta/commons/dao/BaseDao.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import com.lftechnology.vyaguta.commons.pojo.ErrorMessage;
-import com.lftechnology.vyaguta.commons.pojo.ErrorMessageWithAttribute;
 
 /**
  * @author Anish Krishna Manandhar<anishmanandhar@lftechnology.com>
@@ -28,12 +27,11 @@ public abstract class BaseDao {
      */
     public static ErrorMessage constructErrorMessage(Throwable throwable) {
         String message = ExceptionUtils.getRootCauseMessage(throwable);
-        ErrorMessageWithAttribute errorMessage = new ErrorMessageWithAttribute("error", message);
+        ErrorMessage errorMessage = new ErrorMessage( message);
         if (message.contains("duplicate key value violates unique constraint")) {
             message = StringUtils.substringAfter(message, "Detail: Key ");
             String[] messages = message.split("=");
             if (messages != null && messages.length > 1) {
-                errorMessage.setField(messages[0].replaceAll("\\(", "").replaceAll("\\)", ""));
                 errorMessage.setError(messages[1]);
             }
         }
