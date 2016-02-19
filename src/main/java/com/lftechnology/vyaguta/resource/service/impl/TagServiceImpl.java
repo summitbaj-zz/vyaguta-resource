b4798071309979d58a7cdefcbb2a2f1329a176bd
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-19 17:03:24 +0545

    Create implementation class for TagDao

---
+++ src/main/java/com/lftechnology/vyaguta/resource/service/impl/TagServiceImpl.java
@@ -0,0 +1,81 @@
+package com.lftechnology.vyaguta.resource.service.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.lftechnology.vyaguta.commons.exception.ObjectNotFoundException;
+import com.lftechnology.vyaguta.resource.dao.TagDao;
+import com.lftechnology.vyaguta.resource.entity.Tag;
+import com.lftechnology.vyaguta.resource.service.TagService;
+
+/**
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ *
+ */
+public class TagServiceImpl implements TagService {
+
+  @Inject
+  TagDao tagDao;
+
+  @Override
+  public Tag save(Tag tag) {
+    return tagDao.save(tag);
+  }
+
+  @Override
+  public Tag update(Tag tag) {
+    return tagDao.update(tag);
+  }
+
+  @Override
+  public Tag merge(String id, Tag obj) {
+    Tag tag = this.findById(id);
+    if (tag == null) {
+      throw new ObjectNotFoundException();
+    }
+    tag.setTitle(obj.getTitle());
+    return this.update(tag);
+  }
+
+  @Override
+  public void remove(Tag tag) {
+    tagDao.remove(tag);
+
+  }
+
+  @Override
+  public void removeById(String id) {
+    Tag tag = this.findById(id);
+    if (tag == null) {
+      throw new ObjectNotFoundException();
+    }
+    this.remove(tag);
+  }
+
+  @Override
+  public Tag findById(String id) {
+    return tagDao.findById(id);
+  }
+
+  @Override
+  public List<Tag> findAll() {
+    return tagDao.findAll();
+  }
+
+  @Override
+  public Long count() {
+    return tagDao.count();
+  }
+
+  @Override
+  public List<Tag> find(Integer start, Integer offset) {
+    return tagDao.find(start, offset);
+  }
+
+  @Override
+  public List<Tag> findByTitle(String title) {
+    return tagDao.findByTitle(title);
+  }
+}
