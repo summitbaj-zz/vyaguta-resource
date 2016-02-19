f8a5f32194f45d2a32fe7293f01cdf16561d8d4b
krishnatimilsina <krishnatimilsina@lftechnology.com>
2016-02-19 16:03:20 +0545

    VY-RES-6 Rest API for budget type

--- src/main/java/com/lftechnology/vyaguta/resource/rs/BudgetTypeRs.java
+++ src/main/java/com/lftechnology/vyaguta/resource/rs/BudgetTypeRs.java
@@ -1,4 +1,3 @@
-
 package com.lftechnology.vyaguta.resource.rs;
 
 import java.util.List;
@@ -32,66 +31,61 @@ import com.lftechnology.vyaguta.resource.service.BudgetTypeService;
 @Path("/budgettypes")
 public class BudgetTypeRs {
 
-	@Inject
-	BudgetTypeService budgetTypeService;
+  @Inject
+  BudgetTypeService budgetTypeService;
 
-	@Path("/")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response list(
-		@Min(value = 1, message = "Page must be greater than zero.") @QueryParam("page") Integer pageNum,
-		@QueryParam("offset") Integer offset) {
+  @Path("/")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response list(
+      @Min(value = 1, message = "Page must be greater than zero.") @QueryParam("page") Integer pageNum,
+      @QueryParam("offset") Integer offset) {
+    Page page = PageUtil.page(pageNum, offset);
+    List<BudgetType> budgetTypes = budgetTypeService.find(page.getStart(),
+        page.getOffset());
+    return Response.status(Response.Status.OK)
+        .entity(JsonToStringBuilder.toString(budgetTypes)).build();
+  }
 
-		Page page = PageUtil.page(pageNum, offset);
-		List<BudgetType> budgetTypes =
-			budgetTypeService.find(page.getStart(), page.getOffset());
-		return Response.status(Response.Status.OK).entity(
-			JsonToStringBuilder.toString(budgetTypes)).build();
-	}
+  @Path("/")
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response create(@NotNull @Valid BudgetType budgetType) {
+    budgetType = budgetTypeService.save(budgetType);
+    return Response.status(Response.Status.OK)
+        .entity(JsonToStringBuilder.toString(budgetType)).build();
+  }
 
-	@Path("/")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response create(@NotNull @Valid BudgetType budgetType) {
+  @Path("/{id}")
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response update(@PathParam("id") String id,
+      @NotNull @Valid BudgetType budgetTypeNew) {
+    BudgetType budgetType = budgetTypeService.merge(id, budgetTypeNew);
+    return Response.status(Response.Status.OK)
+        .entity(JsonToStringBuilder.toString(budgetType)).build();
+  }
 
-		budgetType = budgetTypeService.save(budgetType);
-		return Response.status(Response.Status.OK).entity(
-			JsonToStringBuilder.toString(budgetType)).build();
-	}
+  @Path("/{id}")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response findById(@PathParam("id") String id) {
+    BudgetType budgetType = budgetTypeService.findById(id);
+    if (budgetType != null) {
+      return Response.status(Response.Status.OK)
+          .entity(JsonToStringBuilder.toString(budgetType)).build();
+    } else {
+      throw new ObjectNotFoundException();
+    }
+  }
 
-	@Path("/{id}")
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response update(@PathParam("id") String id, @NotNull @Valid BudgetType budgetTypeNew) {
-
-		BudgetType budgetType = budgetTypeService.merge(id, budgetTypeNew);
-		return Response.status(Response.Status.OK).entity(
-			JsonToStringBuilder.toString(budgetType)).build();
-	}
-
-	@Path("/{id}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response findById(@PathParam("id") String id) {
-		System.out.println(id);
-		BudgetType budgetType = budgetTypeService.findById(id);
-		if (budgetType != null) {
-			return Response.status(Response.Status.OK).entity(
-				JsonToStringBuilder.toString(budgetType)).build();
-		}
-		else {
-			throw new ObjectNotFoundException();
-		}
-	}
-
-	@Path("/{id}")
-	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response remove(@PathParam("id") String id) {
-
-		budgetTypeService.removeById(id);
-		return Response.status(Response.Status.OK).build();
-	}
+  @Path("/{id}")
+  @DELETE
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response remove(@PathParam("id") String id) {
+    budgetTypeService.removeById(id);
+    return Response.status(Response.Status.OK).build();
+  }
 }
