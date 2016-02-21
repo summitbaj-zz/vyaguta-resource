fe39990c8949459e92b7ec641c556c81d434f7f6
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-21 12:10:50 +0545

    Entity class for Project Status

---
+++ src/main/java/com/lftechnology/vyaguta/resource/entity/ProjectStatus.java
@@ -0,0 +1,32 @@
+package com.lftechnology.vyaguta.resource.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.lftechnology.vyaguta.commons.entity.BaseEntity;
+
+/**
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ *
+ */
+@Entity
+@Table(name = "project_status")
+public class ProjectStatus extends BaseEntity implements Serializable {
+
+  private static final long serialVersionUID = -7761707754863881913L;
+
+  @NotNull(message = "Title cannot be blank.")
+  private String title;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
