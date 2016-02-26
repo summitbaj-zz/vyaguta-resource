f29cf88108603e19dc2fdd0dad2df9d567905a63
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-26 14:42:31 +0545

    Add hashCode and equals method

--- src/main/java/com/lftechnology/vyaguta/resource/entity/Tag.java
+++ src/main/java/com/lftechnology/vyaguta/resource/entity/Tag.java
@@ -48,4 +48,34 @@ public class Tag extends BaseEntity implements Serializable {
         this.project = project;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof Tag)) {
+            return false;
+        }
+        Tag other = (Tag) obj;
+        if (title == null) {
+            if (other.title != null) {
+                return false;
+            }
+        } else if (!title.equals(other.title)) {
+            return false;
+        }
+        return true;
+    }
+
 }
