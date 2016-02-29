a40b492cc1cbb9f03584f0daebf058027f26e5fa
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-02-29 16:36:49 +0545

    Add @Provider for run time visibility

--- src/main/java/com/lftechnology/vyaguta/commons/util/JacksonUnrecognizedExceptionMapper.java
+++ src/main/java/com/lftechnology/vyaguta/commons/util/JacksonUnrecognizedExceptionMapper.java
@@ -3,6 +3,7 @@ package com.lftechnology.vyaguta.commons.util;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -14,7 +15,7 @@ import com.lftechnology.vyaguta.commons.pojo.ErrorMessage;
  * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
  *
  */
-// @Provider
+@Provider
 public class JacksonUnrecognizedExceptionMapper implements ExceptionMapper<UnrecognizedPropertyException> {
 
     @Override
