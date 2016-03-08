890953a26776ccb9c058a44c4ed3ef9c47a6ba8c
bishalshr <bishalshrestha@lftechnology.com>
2016-03-08 10:04:33 +0545

    VY-RES-113 Auto-complete for Account manager with validation

--- frontend/src/js/components/project/AccountManager.js
+++ frontend/src/js/components/project/AccountManager.js
@@ -5,7 +5,6 @@
 
     var ApiUtil = require('../../util/ApiUtil');
     var AutoComplete = require('./Autocomplete');
-    var AUTOCOMPLETE_CLASS = 'autocomplete-suggestions';
 
     var resourceConstant = require('../../constants/resourceConstant');
 
@@ -18,12 +17,10 @@
 
             changeSuggestionState: function (data) {
                 this.setState({suggestions: data});
-                document.getElementsByClassName(AUTOCOMPLETE_CLASS)[0].style.display = 'block';
             },
 
             updateSuggestions: function (input) {
                 this.setState({suggestions: []});
-                document.getElementsByClassName(AUTOCOMPLETE_CLASS)[0].style.display = 'block';
                 ApiUtil.fetchByQuery(resourceConstant.ACCOUNT_MANAGERS, input, this.changeSuggestionState);
             },
 
@@ -32,50 +29,66 @@
 
                 if (key == 13) {
                     event.preventDefault();
-                } else if (key == 32 && !this.refs.inputTag.value) {
-                    event.preventDefault();
                 } else {
                     var inputValue = this.refs.inputTag.value;
                     var pressed = String.fromCharCode(key);
 
-                    inputValue += pressed;
-                    this.updateSuggestions(inputValue.toLowerCase());
+                    if (this.isValid(pressed)) {
+                        this.updateSuggestions(inputValue.toLowerCase());
+                    }
 
                 }
             },
+
+            isValid: function (value) {
+                return /^[a-zA-Z ]+$/.test(value);
+            },
+
             getSuggestionName: function () {
                 var names = [];
                 for (var i = 0; i < this.state.suggestions.length; i++) {
-                    names.push(this.state.suggestions[i].firstName);
+                    names.push(this.getAppendedName(i));
                 }
                 return names;
+            },
+
+            getAppendedName: function (index) {
+                var name;
+                var suggestions = this.state.suggestions;
+                var name = suggestions[index].firstName;
+                if (suggestions[index].middleName) {
+                    name = name.concat(' ', suggestions[index].middleName);
+                }
+                name = name.concat(' ', suggestions[index].lastName);
+                return name;
             },
 
             validateManager: function () {
                 var input = this.refs.inputTag;
                 for (var i = 0; i < this.state.suggestions.length; i++) {
-                    if (input.value === this.state.suggestions[i].firstName) {
+                    if (input.value === this.getAppendedName(i)) {
                         input.parentElement.parentElement.className = 'col-md-6 col-lg-4 element has-success';
-                        this.props.setIsManagerValid(true);
+                        this.props.setManager({'id': this.state.suggestions[i].id});
                         this.refs.availableMessage.innerHTML = 'Valid name';
                         return;
                     }
                 }
                 input.parentElement.parentElement.className += ' has-error';
-                this.props.setIsManagerValid(false);
+                this.props.setManager(null);
                 this.refs.availableMessage.innerHTML = 'Invalid name';
             },
 
             render: function () {
-                var suggestionName = this.getSuggestionName();
+                var suggestionTitle = this.getSuggestionName();
+                console.log(suggestionTitle);
                 return (
                     <div className="col-md-6 col-lg-4 element">
                         <label className="control-label">Account Manager</label>
                         <div className="manager-parent">
                             <input type="text" placeholder="Account Manager Name" ref="inputTag"
-                                   className="form-control manager-input" onKeyDown={this.input}
+                                   className="form-control manager-input" autoComplete="off" onKeyUp={this.input}
                                    onBlur={this.validateManager} id="account-manager"/>
-                            <AutoComplete inputField="manager-input" suggestions={suggestionName}/>
+                            <AutoComplete inputField="manager-input" suggestions={suggestionTitle}/>
                         </div>
                         <span className="help-block" ref="availableMessage"></span>
                     </div>
