fe5bbbd0c7c13b00de8c2f0da8142444f899bbe2
krishnatimilsina <krishnatimilsina@lftechnology.com>
2016-03-09 09:54:12 +0545

    VY-RES-142 formatting

--- src/main/java/com/lftechnology/vyaguta/resource/dao/ClientDao.java
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/ClientDao.java
@@ -3,7 +3,6 @@ package com.lftechnology.vyaguta.resource.dao;
 import com.lftechnology.vyaguta.commons.dao.CrudDao;
 import com.lftechnology.vyaguta.resource.entity.Client;
 
-
 /**
  * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
  */
--- src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ClientDaoImpl.java
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ClientDaoImpl.java
@@ -6,14 +6,13 @@ import com.lftechnology.vyaguta.commons.dao.BaseDao;
 import com.lftechnology.vyaguta.resource.dao.ClientDao;
 import com.lftechnology.vyaguta.resource.entity.Client;
 
-
 /**
  * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
  */
 @Stateless
-public class ClientDaoImpl extends BaseDao<Client, String>implements ClientDao {
+public class ClientDaoImpl extends BaseDao<Client, String> implements ClientDao {
 
-    public ClientDaoImpl() {
-        super(Client.class);
-    }
+  public ClientDaoImpl() {
+    super(Client.class);
+  }
 }
