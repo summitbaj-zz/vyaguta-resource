fb068c2087bf551275026b0ec0df78ed9cbcbde3
krishnatimilsina <krishnatimilsina@lftechnology.com>
2016-03-10 17:39:33 +0545

    VY-RES-181 table for project history

--- resources/database/vyaguta-resource.sql
+++ resources/database/vyaguta-resource.sql
@@ -133,6 +133,22 @@ ALTER TABLE ONLY project_members
 ALTER TABLE ONLY project_members 
 	ADD CONSTRAINT projects_fk FOREIGN KEY(project_id) REFERENCES projects ON DELETE CASCADE;
 	
+CREATE TABLE project_histories (
+    id character varying(32) NOT NULL,
+    project_id character varying(32) NOT NULL,
+    batch_id character varying(32) NOT NULL,
+    attribute character varying(255) NOT NULL,
+    old_value character varying(255),
+    new_value character varying(255),
+    reason text,
+    created_by character varying(32) NOT NULL,
+    updated_by character varying(32),
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone
+);
+ALTER TABLE project_histories OWNER TO frieddust;
+ALTER TABLE ONLY project_histories
+    ADD CONSTRAINT project_histories_pk PRIMARY KEY (id);
 	
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM frieddust;
