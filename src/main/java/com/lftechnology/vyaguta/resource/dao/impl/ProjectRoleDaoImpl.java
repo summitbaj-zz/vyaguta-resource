0621b4e8f09f14bb92a9c6e82c067bc84ea647b4
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-03-29 10:37:12 +0545

    Resolve conflicts

--- src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ProjectRoleDaoImpl.java
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ProjectRoleDaoImpl.java
@@ -1,52 +1,38 @@
 package com.lftechnology.vyaguta.resource.dao.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import javax.ejb.Stateless;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import com.lftechnology.vyaguta.commons.dao.BaseDao;
-import com.lftechnology.vyaguta.commons.util.MultivaluedMap;
-import com.lftechnology.vyaguta.resource.common.CommonConstant;
+import com.lftechnology.vyaguta.commons.jpautil.EntityFilter;
+import com.lftechnology.vyaguta.commons.jpautil.EntitySorter;
 import com.lftechnology.vyaguta.resource.dao.ProjectRoleDao;
 import com.lftechnology.vyaguta.resource.entity.ProjectRole;
+import com.lftechnology.vyaguta.resource.filter.ProjectRoleFilter;
+import com.lftechnology.vyaguta.resource.sort.ProjectRoleSort;
 
 /**
  * @author Krishna Timilsina <krishnatimilsina@lftechnology.com>
  */
 @Stateless
-public class ProjectRoleDaoImpl extends BaseDao<ProjectRole, String> implements ProjectRoleDao {
+public class ProjectRoleDaoImpl extends BaseDao<ProjectRole, String>implements ProjectRoleDao {
+
+    private ProjectRoleFilter projectRoleFilter = new ProjectRoleFilter();
+    private ProjectRoleSort projectRoleSort = new ProjectRoleSort();
 
     public ProjectRoleDaoImpl() {
         super(ProjectRole.class);
     }
 
-    /**
-     * This method defines predicates for Criteria query on the basis of query parameters.
-     * 
-     * @param MultiValued
-     *            <String, String> queryParameters
-     * @param CriteriaBuilder
-     *            criteriaBuilder
-     * @param Root
-     *            <Entity> root
-     * @return Predicate[]
-     */
     @Override
-    protected Predicate[] extractPredicates(MultivaluedMap<String, String> queryParameters, CriteriaBuilder criteriaBuilder,
-            Root<ProjectRole> root) {
-        List<Predicate> predicates = new ArrayList<>();
-
-        // search by title
-        if (queryParameters.containsKey(CommonConstant.TITLE)) {
-            String title = queryParameters.getFirst(CommonConstant.TITLE).toUpperCase();
-            Predicate predicate = criteriaBuilder.equal(criteriaBuilder.upper(root.get(CommonConstant.TITLE)), title);
-            predicates.add(predicate);
-        }
-
-        return predicates.toArray(new Predicate[] {});
+    public Map<String, EntitySorter<ProjectRole>> getSortOperations() {
+        return projectRoleSort.getSortOperations();
     }
+
+    @Override
+    public Map<String, EntityFilter<ProjectRole>> getFilters() {
+        return projectRoleFilter.getFilters();
+    }
+
 }
