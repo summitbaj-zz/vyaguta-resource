54a7a698563868cdc555761c8e58c1463296f232
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-03-29 13:08:04 +0545

    Return null from sort and filter

--- src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ContractDaoImpl.java
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/impl/ContractDaoImpl.java
@@ -7,8 +7,6 @@ import com.lftechnology.vyaguta.commons.jpautil.EntityFilter;
 import com.lftechnology.vyaguta.commons.jpautil.EntitySorter;
 import com.lftechnology.vyaguta.resource.dao.ContractDao;
 import com.lftechnology.vyaguta.resource.entity.Contract;
-import com.lftechnology.vyaguta.resource.filter.ContractFilter;
-import com.lftechnology.vyaguta.resource.sort.ContractSort;
 
 /**
  * 
@@ -17,21 +15,18 @@ import com.lftechnology.vyaguta.resource.sort.ContractSort;
  */
 public class ContractDaoImpl extends BaseDao<Contract, String>implements ContractDao {
 
-    private ContractSort contractSort = new ContractSort();
-    private ContractFilter contractFilter = new ContractFilter();
-
     public ContractDaoImpl(Class<Contract> entityClass) {
         super(Contract.class);
     }
 
     @Override
     public Map<String, EntitySorter<Contract>> getSortOperations() {
-        return contractSort.getSortOperations();
+        return null;
     }
 
     @Override
     public Map<String, EntityFilter<Contract>> getFilters() {
-        return contractFilter.getFilters();
+        return null;
     }
 
 }
