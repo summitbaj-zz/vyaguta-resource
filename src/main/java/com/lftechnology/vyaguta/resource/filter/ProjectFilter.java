54759d8b498312972983b22d3cd7ac945bc82f22
Kailash Bijayananda <kailashraj@lftechnology.com>
2016-04-06 19:01:52 +0545

    update filters

--- src/main/java/com/lftechnology/vyaguta/resource/filter/ProjectFilter.java
+++ src/main/java/com/lftechnology/vyaguta/resource/filter/ProjectFilter.java
@@ -1,6 +1,12 @@
 package com.lftechnology.vyaguta.resource.filter;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+
 import com.lftechnology.vyaguta.commons.jpautil.CommonFilter;
+import com.lftechnology.vyaguta.commons.jpautil.CriteriaMaker;
+import com.lftechnology.vyaguta.commons.jpautil.EntityFilter;
 import com.lftechnology.vyaguta.commons.jpautil.Filterable;
 import com.lftechnology.vyaguta.resource.common.CommonConstant;
 import com.lftechnology.vyaguta.resource.dao.impl.ProjectDaoImpl;
@@ -15,12 +21,25 @@ public class ProjectFilter extends CommonFilter<Project>implements Filterable<Pr
 
     public ProjectFilter() {
         filterBy("title");
+
+        filterByDate(ProjectDaoImpl.START_DATE, ProjectDaoImpl.START_DATE);
+        filterByDate(ProjectDaoImpl.END_DATE, ProjectDaoImpl.END_DATE);
+
+        filterBy("projectType", "projectType|title");
+        filterBy("projectStatus", "projectStatus|title");
+        filterBy("budgetType", "budgetType|title");
+
         searchByField("q", CommonConstant.TITLE);
-        filterByDate(ProjectDaoImpl.START_DATE);
-        filterByDate(ProjectDaoImpl.END_DATE);
-        filterBy("projectType.title");
-        filterBy("projectStatus.title");
-        filterBy("budgetType.title");
+
+        filterMap.put("contract.endDate", new EntityFilter<Project>() {
+
+            @Override
+            public Predicate filter(CriteriaMaker<Project> cm, String field, String value) {
+                Join join = cm.getRoot().join("contracts");
+                Path path = join.get("endDate");
+                return ProjectFilter.this.buildDatePredicate(cm.getCriteriaBuilder(), path, value);
+            }
+        });
     }
 
 }
