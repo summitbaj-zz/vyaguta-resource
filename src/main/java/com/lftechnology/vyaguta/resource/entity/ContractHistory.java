26870c86e1cb51e2eb941d090743defa43f08b3d
Kailash Bijayananda <kailashraj@lftechnology.com>
2016-04-14 11:56:22 +0545

    Fix for: a project can have multiple contracts, a contract can have multiple contract member with same employee

--- src/main/java/com/lftechnology/vyaguta/resource/entity/ContractHistory.java
+++ src/main/java/com/lftechnology/vyaguta/resource/entity/ContractHistory.java
@@ -32,12 +32,14 @@ import com.lftechnology.vyaguta.commons.jpautil.LocalDateSerializer;
 @Entity
 @Table(name = "contract_histories")
 @NamedQueries({
-        @NamedQuery(name = ContractHistory.FIND_BY_PROJECT, query = "SELECT ch FROM ContractHistory ch WHERE ch.project = :project ORDER BY ch.batch.createdAt DESC") })
+        @NamedQuery(name = ContractHistory.FIND_BY_PROJECT, query = "SELECT ch FROM ContractHistory ch WHERE ch.project = :project ORDER BY ch.batch.createdAt DESC"),
+        @NamedQuery(name = ContractHistory.FIND_BY_CONTRACT, query = "SELECT ch FROM ContractHistory ch WHERE ch.contract = :contract ORDER BY ch.batch.createdAt DESC")})
 public class ContractHistory implements Serializable {
 
     private static final long serialVersionUID = -4315662140074205279L;
     private static final String PREFIX = "vyaguta.resource.entity.ContractHistory.";
     public static final String FIND_BY_PROJECT = ContractHistory.PREFIX + "findByProject";
+    public static final String FIND_BY_CONTRACT = ContractHistory.PREFIX + "findByContract";
 
     @Id
     @Type(type = "pg-uuid")
