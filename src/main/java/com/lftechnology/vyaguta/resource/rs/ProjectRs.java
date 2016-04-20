3e811b546b7acc4c9d2c2844b7ed6b17ddecd62c
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-04-20 22:22:05 +0545

    Add api for resourceutilization

--- src/main/java/com/lftechnology/vyaguta/resource/rs/ProjectRs.java
+++ src/main/java/com/lftechnology/vyaguta/resource/rs/ProjectRs.java
@@ -1,10 +1,14 @@
 package com.lftechnology.vyaguta.resource.rs;
 
+import java.awt.image.renderable.ParameterBlock;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.security.RolesAllowed;
+import javax.ejb.Local;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -16,12 +20,15 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import com.lftechnology.vyaguta.commons.Constant;
 import com.lftechnology.vyaguta.commons.exception.ObjectNotFoundException;
+import com.lftechnology.vyaguta.commons.exception.ParameterFormatException;
 import com.lftechnology.vyaguta.commons.util.MultivaluedMapConverter;
 import com.lftechnology.vyaguta.resource.entity.Project;
 import com.lftechnology.vyaguta.resource.service.ProjectHistoryService;
@@ -104,12 +111,20 @@ public class ProjectRs {
         }
     }
 
-    @Path("/resources")
+    @Path("/resourceutilization")
     @GET
-    @RolesAllowed({ "Admin" })
+    @RolesAllowed({ "Employee", "Admin" })
     @Produces(MediaType.APPLICATION_JSON)
-    public Response resource(@Context UriInfo uriInfo) {
-        Map<String, Object> resources = projectService.findAllResource(MultivaluedMapConverter.convert(uriInfo.getQueryParameters()));
+    public Response resourceUtilization(@QueryParam("date") String dateStr) {
+        LocalDate date = LocalDate.now();
+        if (dateStr != null) {
+            try {
+                date = LocalDate.parse(dateStr, Constant.DATE_FORMAT_DB);
+            } catch (DateTimeParseException e) {
+                throw new ParameterFormatException("Invalid date format");
+            }
+        }
+        Map<String, Object> resources = projectService.findResourceUtilization(date);
         return Response.status(Response.Status.OK).entity(resources).build();
     }
 }
