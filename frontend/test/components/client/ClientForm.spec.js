98a66275e70c9a2220502c49d48d2eae28f9a7a4
pratishshr <pratishshrestha@lftechnology.com>
2016-04-20 10:36:58 +0545

    VY-RES-443 Test Cases for client

--- frontend/test/components/client/ClientForm.spec.js
+++ frontend/test/components/client/ClientForm.spec.js
@@ -1,5 +1,150 @@
 /**
  * Created by
  * Pratish Shrestha <pratishshrestha@lftechnology.com>
- * on 4/20/16.
+ * on 4/19/16.
  */
+
+
+//libraries
+import React from 'react';
+import expect from 'expect';
+import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import jsdom from 'jsdom';
+
+//components
+import {WrappedComponent} from '../../../src/js/components/client/ClientForm';
+import ClientRow from '../../../src/js/components/client/ClientRow';
+import store from '../../storeMock';
+
+function setup(params) {
+    var props = {
+        selectedItem: store.getState().crudReducer.selectedItem,
+        apiState: {isRequesting: false, noOfRequests: 0},
+        params: params,
+        actions: {
+            fetchById: expect.createSpy(),
+            clearSelectedItem: expect.createSpy(),
+            apiClearState: expect.createSpy(),
+            updateSelectedItem: expect.createSpy(),
+            addItem: expect.createSpy(),
+            updateItem: expect.createSpy()
+        }
+    }
+
+    var ClientForm = WrappedComponent;
+    var component = mount(<Provider store={store}><ClientForm {...props}/></Provider>);
+
+    return {
+        component: component,
+        actions: props.actions,
+        props: props
+    }
+}
+
+describe('ClientForm component', () => {
+    describe('componentDidMount', () => {
+        it('dispatches fetchById if id is present in the params', () => {
+            var {actions} = setup({id: 123});
+            expect(actions.fetchById).toHaveBeenCalled();
+        });
+
+        it('doesnot dispatch fetchById if id is not present in the params', () => {
+            var {actions} = setup({});
+            expect(actions.fetchById).toNotHaveBeenCalled();
+        });
+    });
+
+    describe('componentWillUnMount', () => {
+        it('dispatches clearSelectedItem when component unmounts', () => {
+            var {component, actions} = setup({});
+            component.unmount();
+            expect(actions.clearSelectedItem).toHaveBeenCalled();
+        });
+
+        it('dispatches apiClearState when component unmounts', () => {
+            var {component, actions} = setup({});
+            component.unmount();
+            expect(actions.apiClearState).toHaveBeenCalled();
+        });
+    });
+
+    describe('handleChange', () => {
+        it('dispatches updateSelectedItem when any changes are made to the name field', () => {
+            var {component, actions} = setup({});
+            var inputField = component.find('#name');
+            inputField.simulate('change');
+            expect(actions.updateSelectedItem).toHaveBeenCalled();
+        });
+
+        it('dispatches updateSelectedItem when any changes are made to the email field', () => {
+            var {component, actions} = setup({});
+            var inputField = component.find('#email');
+            inputField.simulate('change');
+            expect(actions.updateSelectedItem).toHaveBeenCalled();
+        });
+
+        it('dispatches updateSelectedItem when any changes are made to the phoneNo field', () => {
+            var {component, actions} = setup({});
+            var inputField = component.find('#phoneNo');
+            inputField.simulate('change');
+            expect(actions.updateSelectedItem).toHaveBeenCalled();
+        });
+
+        it('dispatches updateSelectedItem when any changes are made to the skype field', () => {
+            var {component, actions} = setup({});
+            var inputField = component.find('#skype');
+            inputField.simulate('change');
+            expect(actions.updateSelectedItem).toHaveBeenCalled();
+        });
+    });
+
+    describe('saveClient', () => {
+        it('does not dispatch any action if form is not valid (with no client name and email)', () => {
+            var {component, actions} = setup({});
+            var form = component.find('form');
+            form.simulate('submit');
+            expect(actions.addItem).toNotHaveBeenCalled();
+            expect(actions.updateItem).toNotHaveBeenCalled();
+        });
+
+        it('does not dispatch any action if form is not valid (with client name but no email)', () => {
+            var {component, actions, props} = setup({});
+            var form = component.find('form');
+            props.selectedItem.clients.name = 'Some Client';
+            form.simulate('submit');
+            expect(actions.addItem).toNotHaveBeenCalled();
+            expect(actions.updateItem).toNotHaveBeenCalled();
+        });
+
+        it('does not dispatch any action if form is not valid (with client email but no name)', () => {
+            var {component, actions, props} = setup({});
+            var form = component.find('form');
+            props.selectedItem.clients.email = 'client@client.com';
+            form.simulate('submit');
+            expect(actions.addItem).toNotHaveBeenCalled();
+            expect(actions.updateItem).toNotHaveBeenCalled();
+        });
+
+        it('dispatches addItem if form is valid and id is not present', () => {
+            var {component, actions, props} = setup({});
+            var form = component.find('form');
+            props.selectedItem.clients.name = 'Some Client';
+            props.selectedItem.clients.email = 'client@client.com';
+            component.update();
+            form.simulate('submit');
+            expect(actions.addItem).toHaveBeenCalled();
+        });
+
+        it('dispatches updateItem if form is valid and id is present', () => {
+            var {component, actions, props} = setup({id: 123});
+            var form = component.find('form');
+            props.selectedItem.clients.name = 'Some Client';
+            props.selectedItem.clients.email = 'client@client.com';
+            component.update();
+            form.simulate('submit');
+            expect(actions.updateItem).toHaveBeenCalled();
+        });
+
+    });
+})
---
+++ frontend/test/components/client/ClientList.spec.js
@@ -0,0 +1,127 @@
+/**
+ * Created by
+ * Pratish Shrestha <pratishshrestha@lftechnology.com>
+ * on 4/19/16.
+ */
+
+//libraries
+import React from 'react';
+import expect from 'expect';
+import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import jsdom from 'jsdom';
+
+//components
+import {WrappedComponent} from '../../../src/js/components/client/ClientList';
+import ClientRow from '../../../src/js/components/client/ClientRow';
+import store from '../../storeMock';
+
+function setup() {
+    var props = {
+        clients: [{id: 1, title: 'client1'}, {id: 2, title: 'client2'}],
+        pagination: {},
+        actions: {
+            fetchByQuery: expect.createSpy(),
+            clearPagination: expect.createSpy(),
+            apiClearState: expect.createSpy(),
+            deleteItem: expect.createSpy()
+        }
+    }
+    var ClientList = WrappedComponent;
+    var component = mount(<Provider store={store}><ClientList {...props}/></Provider>);
+
+    return {
+        component: component,
+        clients: props.clients,
+        actions: props.actions,
+        clientRow: component.find(ClientRow)
+    }
+}
+
+describe('ClientList Component', () => {
+    describe('componentDidMount', () => {
+        it('dispatches fetchByQuery action on componentDidMount', () => {
+            var {actions} = setup();
+            expect(actions.fetchByQuery).toHaveBeenCalled();
+        });
+    });
+
+    describe('componentWillUnmount', () => {
+        it('dispatches clearPagination action when component unmounts', () => {
+            var {component, actions} = setup();
+            component.unmount();
+            expect(actions.clearPagination).toHaveBeenCalled();
+        });
+
+        it('dispatches apiClearState action when component unmounts', () => {
+            var {component, actions} = setup();
+            component.unmount();
+            expect(actions.apiClearState).toHaveBeenCalled();
+        });
+    });
+
+    describe('renderClient', () => {
+        it('renders all the client rows', () => {
+            var {clientRow, clients} = setup();
+            expect(clientRow.length).toEqual(clients.length);
+        });
+    });
+
+    describe('sort', () => {
+        it('calls sort() function when client name heading is clicked', () => {
+            var {actions, component} = setup();
+            var clientName = component.find('#name');
+            clientName.simulate('click');
+            expect(actions.fetchByQuery).toHaveBeenCalled();
+        });
+
+        it('calls sort() function when client email heading is clicked', () => {
+            var {actions, component} = setup();
+            var clientEmail = component.find('#email');
+            clientEmail.simulate('click');
+            expect(actions.fetchByQuery).toHaveBeenCalled();
+        });
+
+        it('calls sort() function when client phoneNo heading is clicked', () => {
+            var {actions, component} = setup();
+            var clientPhoneNo = component.find('#phoneNo');
+            clientPhoneNo.simulate('click');
+            expect(actions.fetchByQuery).toHaveBeenCalled();
+        });
+
+        it('calls sort() function when client skype heading is clicked', () => {
+            var {actions, component} = setup();
+            var clientSkype = component.find('#skype');
+            clientSkype.simulate('click');
+            expect(actions.fetchByQuery).toHaveBeenCalled();
+        });
+    })
+
+    describe('deleteClient', () => {
+        it('opens a confirm box when delete button is pressed', () => {
+            var {actions, component, clientRow} = setup();
+            var deleteButton = clientRow.find('button').first();
+            deleteButton.simulate('click');
+            var confirmBox = $('.jconfirm-box');
+            expect(confirmBox.length).toBeGreaterThan(0);
+        });
+
+        it('dispatches deleteItem action when yes button is pressed in confirm box', () => {
+            var {actions, component, clientRow} = setup();
+            var deleteButton = clientRow.find('button').first();
+            deleteButton.simulate('click');
+            var yesButton = $('.jconfirm-box .btn-success');
+            yesButton.click();
+            expect(actions.deleteItem).toHaveBeenCalled();
+        });
+
+        it('does not dispatch deleteItem action when cancel button is pressed in confirm box', () => {
+            var {actions, component, clientRow} = setup();
+            var deleteButton = clientRow.find('button').first();
+            deleteButton.simulate('click');
+            var cancelButton = $('.jconfirm-box .btn-danger');
+            cancelButton.click();
+            expect(actions.deleteItem).toNotHaveBeenCalled();
+        });
+    });
+});
