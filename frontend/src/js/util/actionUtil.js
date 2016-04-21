0e3c4d8d1ea129c1e173a1d67d402527c12bf49d
bishalshr <bishalshrestha@lftechnology.com>
2016-04-21 11:17:07 +0545

    VY-RES-459 Change taostr message on unsuccessful deletion

--- frontend/src/js/util/actionUtil.js
+++ frontend/src/js/util/actionUtil.js
@@ -9,30 +9,37 @@
 
     //constants
     var urlConstant = require('../constants/urlConstant');
+    var messageConstant = require('../constants/messageConstant');
 
     //libraries
     var Toastr = require('toastr');
 
     var actionUtil = {
-            responseError: function (dispatch, error, entity, callback) {
-                if (error.status == 401) {
-                    dispatch(apiActions.apiRequest(entity));
-                    apiUtil.refreshSession().then(function (response) {
-                        dispatch(apiActions.apiResponse(entity));
-                        dispatch(callback);
-                    });
-                } else if (error.status == 404) {
-                    browserHistory.push(urlConstant.PAGE_NOT_FOUND);
-                } else {
-                    Toastr.error(error.response.body.error || error.response.body[0].error);
+        responseError: function (dispatch, error, entity, callback) {
+            if (error.status == 401) {
+                dispatch(apiActions.apiRequest(entity));
+                apiUtil.refreshSession().then(function (response) {
+                    dispatch(apiActions.apiResponse(entity));
+                    dispatch(callback);
+                });
+            } else if (error.status == 404) {
+                browserHistory.push(urlConstant.PAGE_NOT_FOUND);
+            } else if (error.status == 400) {
+                var message = error.response.body.error || error.response.body[0].error;
+                if (message.indexOf(messageConstant.FOREIGN_KEY_VIOLATION_MESSAGE) > -1) {
+                    message = messageConstant.UNSUCCESSFUL_DELETE_MESSAGE;
                 }
-            },
-
-            isSameRoute: function (state, oldRoute) {
-                var currentRoute = state().routing.locationBeforeTransitions.pathname;
-                return oldRoute == currentRoute;
+                Toastr.error(message);
+            } else {
+                Toastr.error(error.response.body.error || error.response.body[0].error);
             }
-        };
+        },
+
+        isSameRoute: function (state, oldRoute) {
+            var currentRoute = state().routing.locationBeforeTransitions.pathname;
+            return oldRoute == currentRoute;
+        }
+    };
 
     module.exports = actionUtil;
 
