8eeebc511cebb41ea046446922390aa2ded9ded7
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-04-22 22:11:38 +0545

    VY-RES-462 Remove unused imports

--- src/main/java/com/lftechnology/vyaguta/resource/rs/ProjectRs.java
+++ src/main/java/com/lftechnology/vyaguta/resource/rs/ProjectRs.java
@@ -25,7 +25,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.hibernate.validator.constraints.NotBlank;
 import org.slf4j.Logger;
 
 import com.lftechnology.vyaguta.commons.Constant;
