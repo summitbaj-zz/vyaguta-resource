11b31ec892daf2ac54a48e5b20b2eed403c66e31
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-04-26 16:06:49 +0545

    VY-RES-462 Implement hashCode

--- src/main/java/com/lftechnology/vyaguta/resource/pojo/AvailableResource.java
+++ src/main/java/com/lftechnology/vyaguta/resource/pojo/AvailableResource.java
@@ -73,4 +73,34 @@ public class AvailableResource {
         this.designation = designation;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof AvailableResource)) {
+            return false;
+        }
+        AvailableResource other = (AvailableResource) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+
 }
