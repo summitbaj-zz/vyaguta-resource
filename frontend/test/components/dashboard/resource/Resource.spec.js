e5b06b051364b142d38319200c5d603e7d7c71dc
bishalshr <bishalshrestha@lftechnology.com>
2016-05-02 10:50:53 +0545

    VY-RES-475 Write test cases for resource

--- frontend/test/components/dashboard/resource/Resource.spec.js
+++ frontend/test/components/dashboard/resource/Resource.spec.js
@@ -1,36 +1,31 @@
-/*
 import React from 'react';
 import expect from 'expect';
 import {shallow, mount} from 'enzyme';
 
 //components
 import Resource from '../../../../src/js/components/dashboard/resource/Resource';
+import dashboardUtil from '../../../../src/js/util/dashboardUtil';
+
+dashboardUtil.calculatePercentage = expect.createSpy().andReturn('50%');
 
 function setup() {
     var props = {
         resource: {totalResource: 5, freeResource: 2, bookedResource: {bookedResourceCount: 3, billed: 2, unbilled: 3}}
     }
 
-    var component = mount(<OverdueProjects {...props}/>);
+    var component = mount(<Resource {...props}/>);
     return {
         component: component,
         props: props
     }
 }
 
-describe('OverdueProjects component', () => {
-    describe('renderOverdueProject', () => {
-        it('returns correct component', () => {
-            var {component, props} = setup();
-            var actual = component.instance().renderOverdueProject(0);
-            var expected = <OverdueProjectRow key="0" overdueProject={props.overdueProjects[0]}/>;
-            expect(actual).toEqual(expected);
-        });
-
-        it('renders all overdue projects', () => {
-            var {component, props} = setup();
-            var total = component.find(OverdueProjectRow).length;
-            expect(total).toEqual(props.overdueProjects.length);
+describe('Resource component', () => {
+    describe('render', () => {
+        it('calls calculatePercentage of dashboardUtil', () => {
+            var {props} = setup();
+            expect(dashboardUtil.calculatePercentage).toHaveBeenCalledWith(props.resource.bookedResource.billed, props.resource.bookedResource.bookedResourceCount);
+            expect(dashboardUtil.calculatePercentage).toHaveBeenCalledWith(props.resource.bookedResource.unbilled, props.resource.bookedResource.bookedResourceCount);
         });
     });
-});*/
+});
