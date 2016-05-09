ea6337c1c3925de28b52af4390440051dd9a2530
Prajjwal Kandel <prajjwalkandel@lftechnology.com>
2016-05-09 18:06:56 +0545

    Add service for entity OperationalResource

---
+++ src/main/java/com/lftechnology/vyaguta/resource/service/OperationalResourceService.java
@@ -0,0 +1,10 @@
+package com.lftechnology.vyaguta.resource.service;
+
+import java.util.UUID;
+
+import com.lftechnology.vyaguta.commons.service.CrudService;
+import com.lftechnology.vyaguta.resource.entity.OperationalResource;
+
+public interface OperationalResourceService extends CrudService<OperationalResource, UUID>{
+
+}
