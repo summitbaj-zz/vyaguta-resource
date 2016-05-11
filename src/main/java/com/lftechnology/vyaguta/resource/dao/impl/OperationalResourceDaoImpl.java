de0f484865f7e6bc7c0fd7a068aee998fb4e70b4
Prajjwal Kandel <prajjwalkandel@lftechnology.com>
2016-05-11 12:54:37 +0545

    Include date in the query for finding billed and unbilled operational resources

--- src/main/java/com/lftechnology/vyaguta/resource/dao/impl/OperationalResourceDaoImpl.java
+++ src/main/java/com/lftechnology/vyaguta/resource/dao/impl/OperationalResourceDaoImpl.java
@@ -35,8 +35,8 @@ public class OperationalResourceDaoImpl extends BaseDao<OperationalResource, UUI
     public Map<String, Object> findBilledAndUnbilledResource(LocalDate date) {
         Map<String, Object> map = new HashMap<>();
         Query query =
-                em.createNativeQuery("SELECT SUM(CASE WHEN cm.billed = 't' THEN (cm.allocation * 0.01) ELSE 0 END) AS Billed, SUM(CASE WHEN cm.billed = 'f' THEN (cm.allocation * 0.01) ELSE 0 END) AS Unbilled FROM contract_members cm JOIN operational_resources ors ON cm.employee_id = ors.employee_id");
-//        query.setParameter("date", date);
+                em.createNativeQuery("SELECT SUM(CASE WHEN cm.billed = 't' THEN (cm.allocation * 0.01) ELSE 0 END) AS Billed, SUM(CASE WHEN cm.billed = 'f' THEN (cm.allocation * 0.01) ELSE 0 END) AS Unbilled FROM contract_members cm JOIN operational_resources ors ON cm.employee_id = ors.employee_id WHERE :date BETWEEN join_date AND end_date");
+        query.setParameter("date", date);
         List<Object[]> result = query.getResultList();
         for (Object[] obj : result) {
             map.put("billed", obj[0] == null ? 0.0 : obj[0]);
