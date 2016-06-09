bee6a691aa711d28001269323b5643d0ee363787
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-06-09 10:21:06 +0545

    VY-RES-530 Define UserRole entity

---
+++ src/main/java/com/lftechnology/vyaguta/resource/entity/UserRole.java
@@ -0,0 +1,189 @@
+package com.lftechnology.vyaguta.resource.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.lftechnology.vyaguta.commons.SecurityRequestContext;
+import com.lftechnology.vyaguta.commons.jpautil.GuidUtil;
+import com.lftechnology.vyaguta.commons.jpautil.LocalDateTimeAttributeConverter;
+import com.lftechnology.vyaguta.commons.jpautil.LocalDateTimeDeserializer;
+import com.lftechnology.vyaguta.commons.jpautil.LocalDateTimeSerializer;
+import com.lftechnology.vyaguta.commons.jpautil.UserDeserializer;
+import com.lftechnology.vyaguta.commons.jpautil.UserSerializer;
+import com.lftechnology.vyaguta.commons.pojo.User;
+
+/**
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ *
+ */
+@Entity
+@Table(name = "users_roles")
+@NamedQueries({ @NamedQuery(name = UserRole.FIND_ROLES_OF_USER, query = "SELECT ur.role FROM UserRole ur WHERE ur.user = :user") })
+public class UserRole implements Serializable {
+
+    private static final String PREFIX = "vyaguta.resource.entity.";
+    public static final String FIND_ROLES_OF_USER = PREFIX + "findRolesOfUser";
+    private static final long serialVersionUID = 5570040655149321298L;
+
+    @Id
+    @Type(type = "pg-uuid")
+    protected UUID id;
+
+    @AttributeOverrides(@AttributeOverride(name = "id", column = @Column(name = "user_id") ))
+    @JsonDeserialize(using = UserDeserializer.class)
+    @JsonSerialize(using = UserSerializer.class)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
+    @AttributeOverrides(@AttributeOverride(name = "id", column = @Column(name = "created_by") ))
+    @JsonDeserialize(using = UserDeserializer.class)
+    @JsonSerialize(using = UserSerializer.class)
+    private User createdBy;
+
+    @AttributeOverrides(@AttributeOverride(name = "id", column = @Column(name = "updated_by") ))
+    @JsonDeserialize(using = UserDeserializer.class)
+    @JsonSerialize(using = UserSerializer.class)
+    private User updatedBy;
+
+    @Column(name = "created_at", columnDefinition = "uuid")
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime updatedAt;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public User getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(User updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof UserRole)) {
+            return false;
+        }
+        UserRole other = (UserRole) obj;
+        if (role == null) {
+            if (other.role != null) {
+                return false;
+            }
+        } else if (!role.equals(other.role)) {
+            return false;
+        }
+        if (user == null) {
+            if (other.user != null) {
+                return false;
+            }
+        } else if (!user.equals(other.user)) {
+            return false;
+        }
+        return true;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.setId(GuidUtil.generate());
+        this.setCreatedAt(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.setUpdatedAt(LocalDateTime.now());
+        this.setUpdatedBy(SecurityRequestContext.getCurrentUser());
+    }
+
+}
